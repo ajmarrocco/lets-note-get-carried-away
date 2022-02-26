c67951f0f44d5e87a58b3c2f9e6abe20324839ac
ajmarrocco <ajmarrocco@gmail.com>
2022-02-25 21:20:35 -0500

    add notes constant and updated db.json

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+// initialize express
 const express = require('express');
-
+// gives exact destination on the host
 const PORT = process.env.PORT || 3001;
+// instantiate the server
 const app = express();
+// initalizes note constant
+const { notes } = require('./Develop/db/db');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+    res.json(notes);
+});
+
+//makes server listen
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
